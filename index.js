0ed93dfbb6f4c962fa452648d4600546e50fd04a
Clae <56447997+clayruh@users.noreply.github.com>
2024-03-12 14:20:19 -0400

    write more functions

--- index.js
+++ index.js
@@ -4,5 +4,44 @@ function Monday() {
 }
 
 function Tuesday() {
-    console.log("Go for a file-mile run")
+    console.log("Go for a file-mile run");
+    console.log("Swim 40 laps");
 }
+
+function Wednesday() {
+    console.log("Go for a five-mile run")
+    console.log("Go for a five-mile run")
+}
+
+function Thursday() {
+    console.log("Go for a five-mile run")
+    console.log("Pump iron")
+}
+
+function Friday() {
+    console.log("Go for five-mile run")
+    console.log("Swim 40 laps")
+}
+
+function runFiveMiles(){
+    console.log("Go for a five-mile run")
+}
+
+function exerciseRoutine(postRunActivity) {
+    runFiveMiles()
+    postRunActivity()
+}
+
+function Monday(){
+    exerciseRoutine(liftWeights)
+}
+
+exerciseRoutine(function () {
+    console.log("Stretch! Work that core!")
+} )
+
+exerciseRoutine( () => {
+    console.log("Stretch! Work that core!")
+} )
+
+exerciseRoutine( () => console.log("Stretch! Work that core!") )
